ba741649b57497cd1c09a9b4415340aefa9bbc3b
ltd <qugar04@gmail.com>
2025-06-28 21:57:48 +0300

    task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 const sortByDescendingFriendCount = users =>
-  users.toSorted((a, b) => a.friends.length - b.friends.length);
+  users.toSorted((a, b) => b.friends.length - a.friends.length);
 
 console.log(
   sortByDescendingFriendCount([
